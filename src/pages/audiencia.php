6a760a09b7f2b8cd75321120034e5f22d269d3f7
dimasfonseca <82244909+dimasfonseca@users.noreply.github.com>
2023-03-04 10:51:02 -0300

    finalização do front

--- src/pages/audiencia.php
+++ src/pages/audiencia.php
@@ -7,7 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- <link rel="stylesheet" type="text/css" href="../style/common.css"> -->
     <link rel="stylesheet" type="text/css" href="../style/audiencia.css">
 
     <title>Audiência</title>
@@ -147,39 +146,43 @@
             <h3>Mérito da Audiência</h3>
             <form>
                 <div class="conteiner-linha">
-                    <p class="negrito">É cabível a transação penal?</p>
-                    <input type="radio" id="cabe-sim" name="cabimento-tp" value="Sim">
-                    <label for="cabe-sim">Sim</label>
-                    <input type="radio" id="cabe-nao" name="cabimento-tp" value="Não">
-                    <label for="cabe-nao">Não</label>
+                    <p class="margem-abaixo-5 negrito">É cabível a transação penal?</p>
+                    <div class="margem-esquerda-10">
+                        <input type="radio" id="cabimento-sim" name="cabimento-tp" value="Sim">
+                        <label for="cabimento-sim">Sim</label>
+                    </div>
+                    <div class="margem-esquerda-10 margem-abaixo-25">
+                        <input type="radio" id="cabimento-nao" name="cabimento-tp" value="Não">
+                        <label for="cabimento-nao">Não</label>
+                    </div>
                 </div>
 
                 <div class="conteiner-linha">
                     <p class="negrito">Proposta formulada pelo Ministério Público:</p>
-                    <div class="conteiner-coluna">
+                    <div class="conteiner-coluna margem-esquerda-10">
                         <div class="conteiner-linha">
                             <input type="radio" id="afastamento" name="proposta" value="Afastamento">
-                            <label for="afastamento">Afastamento dos estádios</label>
+                            <label for="afastamento" class="margem-esquerda-10">Afastamento dos estádios</label>
                         </div>
                         <div class="conteiner-linha">
                             <input type="radio" id="pp" name="proposta" value="Prestação Pecuniária">
-                            <label for="pp">Prestação Pecuniária</label>
+                            <label for="pp" class="margem-esquerda-10">Prestação Pecuniária</label>
                         </div>
-                        <div class="conteiner-linha">    
+                        <div class="conteiner-linha margem-abaixo-25">    
                             <input type="radio" id="psc" name="proposta" value="Prestação de Serviços à Comunidade">
-                            <label for="psc">Prestação de Serviços à Comunidade</label>
+                            <label for="psc" class="margem-esquerda-10">Prestação de Serviços à Comunidade</label>
                         </div>                    
                     </div>
                 </div>
 
                 <div class="conteiner-coluna">
-                    <p class="negrito">Afastamento dos estádios</p>
+                    <p class="margem-abaixo-5 negrito">Afastamento dos estádios</p>
                     <div class="conteiner-coluna">
-                        <div class="conteiner-linha">
+                        <div class="conteiner-linha margem-esquerda-10">
                             <label for="time-afastamento">Time: </label>
                             <input type="text" id="time-afastamento" name="time-afastamento" placeholder="São Paulo (exemplo)">
                         </div>   
-                        <div class="conteiner-linha">
+                        <div class="conteiner-linha margem-esquerda-10 margem-abaixo-25">
                             <label for="prazo-afastamento">Prazo (em meses): </label>
                             <input type="text" id="prazo-afastamento" name="prazo-afastamento" placeholder="60 (exemplo)">
                         </div>
@@ -187,13 +190,13 @@
                 </div>
 
                 <div class="conteiner-coluna">
-                    <p class="negrito">Prestação de Serviços à Comunidade</p>
+                    <p class="margem-abaixo-5 negrito">Prestação de Serviços à Comunidade</p>
                     <div class="conteiner-coluna">
-                        <div class="conteiner-linha">
+                        <div class="conteiner-linha margem-esquerda-10">
                             <label for="horas-psc">Quantidade de horas a cumprir: </label>
                             <input type="text" id="horas-psc" name="horas-psc" placeholder="28 (exemplo)">
                         </div>
-                        <div class="conteiner-linha">
+                        <div class="conteiner-linha margem-esquerda-10 margem-abaixo-25">
                             <label for="prazopsc">Prazo para cumprimento (em dias): </label>
                             <input type="text" id="prazo-psc" name="prazo-psc" placeholder="30 (exemplo)">
                         </div>
@@ -201,13 +204,13 @@
                 </div>
 
                 <div class="conteiner-coluna">
-                    <p class="negrito">Prestação pecuniária</p>
+                    <p class="margem-abaixo-5 negrito">Prestação pecuniária</p>
                     <div class="conteiner-coluna">
-                        <div class="conteiner-linha">
+                        <div class="conteiner-linha margem-esquerda-10">
                             <label for="valor-pp">Valor: R$ </label>
                             <input type="text" id="valor-pp" name="valor-pp" placeholder="606,00 (exemplo)">
                         </div>
-                        <div class="conteiner-linha">
+                        <div class="conteiner-linha margem-esquerda-10 margem-abaixo-25">
                             <label for="prazo-pp">Prazo (em dias): </label>
                             <input type="text" id="prazo-pp" name="prazo-pp" placeholder="60 (exemplo)">
                         </div>
@@ -215,11 +218,15 @@
                 </div>
                 
                 <div class="conteiner-linha">
-                    <p class="negrito">Manifestação do Réu:</p>
-                    <input type="radio" id="aceita-sim" name="aceitacao" value="Aceita">
-                    <label for="aceita-sim">Aceita</label>
-                    <input type="radio" id="aceita-nao" name="aceitacao" value="Não aceita">
-                    <label for="aceita-nao">Não aceita</label>
+                    <p class="margem-abaixo-5 negrito">Manifestação do Réu:</p>
+                    <div class="margem-esquerda-10">
+                        <input type="radio" id="aceita-sim" name="aceitacao" value="Aceita">
+                        <label for="aceita-sim">Aceita</label>
+                    </div>
+                    <div class="margem-esquerda-10 margem-abaixo-25">
+                        <input type="radio" id="aceita-nao" name="aceitacao" value="Não aceita">
+                        <label for="aceita-nao">Não aceita</label>
+                    </div>
                 </div>
 
             </form>
