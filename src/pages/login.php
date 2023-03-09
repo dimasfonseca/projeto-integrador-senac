56e88504262993de1b218fc66abe929ff0fa2fd5
Yan Barbosa <yanlucca93@gmail.com>
2023-03-09 15:07:38 -0300

    mensagem de erro

--- src/pages/login.php
+++ src/pages/login.php
@@ -9,48 +9,7 @@ if (!array_key_exists('reqs', $_SESSION)) {
     $_SESSION['reqs']++;
 }
 
-include("conexao.php");
-
-if(isset($_POST['email']) || isset($_POST['senha'])) {
-
-    if(strlen($_POST['email']) == 0) {
-        echo "Preencha seu e-mail"; /*josevinicius@tjsp.jus.br*/
-    } else if(strlen($_POST['senha']) == 0) {
-        echo "Preencha sua senha"; /*tjsp12345*/
-    } else {
-
-        $email = $mysqli->real_escape_string($_POST['email']);
-        $senha = $mysqli->real_escape_string($_POST['senha']); 
-
-        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
-
-        $quantidade = $sql_query->num_rows;
-
-        if($quantidade == 1) { 
-
-            $usuario = $sql_query->fetch_assoc();
-
-            if(!isset($_SESSION)) {
-                session_start();
-            }
-
-            $_SESSION['id'] = $usuario['id'];
-            $_SESSION['nome'] = $usuario['nome'];
-
-            header("Location: audiencia.php");
-            
-
-
-        } else {
-            echo "Falha ao logar! E-mail ou senha incorretos";
-        }
-    }
-
-}
-
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -59,7 +18,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link rel="stylesheet" type="text/css" href="../style/index.css">
+    <link rel="stylesheet" type="text/css" href="../style/login.css">
 
     <title>Login</title>
 </head>
@@ -102,12 +61,46 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
             <button type="submit">Entrar</button>
         </p>
         </form>
+<?php        
+        include("conexao.php");
 
-<!--         <a href="./audiencia.php"><button>Entrar</button></a>
+if(isset($_POST['email']) || isset($_POST['senha'])) {
 
-        <p id="erro-login"> 
-            Usuário ou senha inválidos
-        </p> -->
+    if(strlen($_POST['email']) == 0) {
+        echo '<p id="erro-login">Preencha seu e-mail</p>'; /*josevinicius@tjsp.jus.br*/
+    } else if(strlen($_POST['senha']) == 0) {
+        echo '<p id="erro-login">Preencha sua senha</p>'; /*tjsp12345*/
+    } else {
+
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']); 
+
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1) { 
+
+            $usuario = $sql_query->fetch_assoc();
+
+            if(!isset($_SESSION)) {
+                session_start();
+            }
+
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("Location: audiencia.php");
+            
+
+
+        } else {
+            echo '<p id="erro-login">Falha ao logar! E-mail ou senha incorretos</p>';
+        }
+    }
+}
+?>
     </main>
 
     <footer>
